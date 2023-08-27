83f62d5fcb534ce74a8be34f2ca3c3304a9effa3
Cider Boi <me@ciderboi.xyz>
2023-08-28 00:19:28 +0530

    Added VCS

---
+++ app/api/chat/route.ts
@@ -0,0 +1,68 @@
+// @ts-ignore
+import { kv } from '@vercel/kv'
+import { OpenAIStream, StreamingTextResponse, HuggingFaceStream } from 'ai'
+import { experimental_buildOpenAssistantPrompt } from 'ai/prompts'
+import { HfInference } from '@huggingface/inference'
+
+import { auth } from '@/auth'
+import { nanoid } from '@/lib/utils'
+import exp from 'constants'
+
+export const runtime = 'edge'
+
+const Hf = new HfInference(process.env.HUGGINGFACE_API_KEY)
+
+export async function POST(req: Request) {
+  const json = await req.json()
+  const { messages, previewToken } = json
+  const userId = (await auth())?.user.id
+
+  if (!userId) {
+    return new Response('Unauthorized', {
+      status: 401
+    })
+  }
+
+  const res = await Hf.textGenerationStream({
+    model: 'OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5',
+    inputs: experimental_buildOpenAssistantPrompt(messages),
+    parameters: {
+      max_new_tokens: 1000,
+      // @ts-ignore (this is a valid parameter specifically in OpenAssistant models)
+      typical_p: 0.5,
+      repetition_penalty: 1,
+      truncate: 1000,
+      return_full_text: false
+    }
+  })
+
+  const stream = HuggingFaceStream(res, {
+    async onCompletion(completion) {
+      const title = json.messages[0].content.substring(0, 100)
+      const id = json.id ?? nanoid()
+      const createdAt = Date.now()
+      const path = `/chat/${id}`
+      const payload = {
+        id,
+        title,
+        userId,
+        createdAt,
+        path,
+        messages: [
+          ...messages,
+          {
+            content: completion,
+            role: 'assistant'
+          }
+        ]
+      }
+      await kv.hmset(`chat:${id}`, payload)
+      await kv.zadd(`user:chat:${userId}`, {
+        score: createdAt,
+        member: `chat:${id}`
+      })
+    }
+  })
+
+  return new StreamingTextResponse(stream)
+}
---
+++ components/clear-history.tsx
@@ -0,0 +1,82 @@
+'use client'
+
+import * as React from 'react'
+import { useRouter } from 'next/navigation'
+import { toast } from 'react-hot-toast'
+
+import { ServerActionResult } from '@/lib/types'
+import { Button } from '@/components/ui/button'
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger
+} from '@/components/ui/alert-dialog'
+import { IconSpinner } from '@/components/ui/icons'
+
+interface ClearHistoryProps {
+  clearChats: () => ServerActionResult<void>
+}
+
+export function ClearHistory({ clearChats }: ClearHistoryProps) {
+  const [open, setOpen] = React.useState(false)
+  const [isPending, startTransition] = React.useTransition()
+  const router = useRouter()
+
+  return (
+    <div className='text-white'>
+      <AlertDialog open={open} onOpenChange={setOpen}>
+        <AlertDialogTrigger asChild>
+          {/* @ts-ignore */}
+          <Button variant="ghost" disabled={isPending} className='text-white'>
+            {isPending && <IconSpinner className="mr-2" />}
+            Clear history
+          </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>
+              <span className='text-white'>
+                Are you absolutely sure?
+              </span>
+            </AlertDialogTitle>
+            <AlertDialogDescription>
+              This will permanently delete your chat history and remove your data
+              from our servers.
+            </AlertDialogDescription>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel disabled={isPending}>              <span className='text-white'>
+              Cancel
+            </span></AlertDialogCancel>
+            <AlertDialogAction
+              disabled={isPending}
+              onClick={(event: any) => {
+                event.preventDefault()
+                startTransition(async () => {
+                  const result = await clearChats()
+
+                  if (result && 'error' in result) {
+                    toast.error(result.error)
+                    return
+                  }
+
+                  setOpen(false)
+                  router.push('/')
+                })
+              }}
+            >
+              {isPending && <IconSpinner className="mr-2 animate-spin" />}
+              Delete
+            </AlertDialogAction>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
+    </div>
+  )
+}
